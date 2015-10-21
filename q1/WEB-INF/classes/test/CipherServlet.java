208a652352bb73b4fe1ff80168ac26ed6dfaa49e
Neha Arora <neha.arora.cs@gmail.com>
2015-10-20 17:42:22 -0700

    Initial successful t2.micro run 10.19

---
+++ q1/WEB-INF/classes/test/CipherServlet.java
@@ -0,0 +1,34 @@
+package test;
+
+import java.io.*;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+import javax.servlet.http.*;
+import javax.servlet.*;
+
+public class CipherServlet extends HttpServlet {
+  private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	
+  public void doGet (HttpServletRequest req,
+                     HttpServletResponse res)
+    throws ServletException, IOException
+  {
+    String key = req.getParameter("key");
+    String msg = req.getParameter("message");
+    char[] dec = null;
+    if ((key != null) && (msg != null)) {
+      try {
+        Cipher c = new Cipher(key, msg);
+        dec = c.decrypt();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+    PrintWriter out = res.getWriter();
+    out.println("TRINITY,9807-6280-2282");
+    out.println(sdf.format(new Date()));
+    out.println(dec);
+    out.close();
+  }
+}
