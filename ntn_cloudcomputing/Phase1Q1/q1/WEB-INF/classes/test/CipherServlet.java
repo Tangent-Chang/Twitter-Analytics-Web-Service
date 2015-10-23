2fb7e66bc422ae1e8db0c8042a7df93ec72b962f
Neha Arora <neha.arora.cs@gmail.com>
2015-10-22 17:46:07 -0700

    updates to EST date processing

--- ntn_cloudcomputing/Phase1Q1/q1/WEB-INF/classes/test/CipherServlet.java
+++ ntn_cloudcomputing/Phase1Q1/q1/WEB-INF/classes/test/CipherServlet.java
@@ -2,14 +2,16 @@ package test;
 
 import java.io.*;
 import java.util.Date;
+import java.util.TimeZone;
 import java.text.SimpleDateFormat;
 
 import javax.servlet.http.*;
 import javax.servlet.*;
 
 public class CipherServlet extends HttpServlet {
-  private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-	
+
+  private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+  
   public void doGet (HttpServletRequest req,
                      HttpServletResponse res)
     throws ServletException, IOException
@@ -27,8 +29,12 @@ public class CipherServlet extends HttpServlet {
     }
     PrintWriter out = res.getWriter();
     out.println("TRINITY,9807-6280-2282");
+    TimeZone utcEast= TimeZone.getTimeZone("EST");
+    sdf.setTimeZone(utcEast);
     out.println(sdf.format(new Date()));
     out.println(dec);
     out.close();
+   
   }
+
 }
