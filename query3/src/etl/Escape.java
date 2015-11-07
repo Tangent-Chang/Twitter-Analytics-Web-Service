d2dcae337dd1fa71302d9e1be06ca128bb3c1ba8
Neha Arora <neha.arora.cs@gmail.com>
2015-11-06 23:40:07 -0800

    Escape the escaper\!

--- query3/src/etl/Escape.java
+++ query3/src/etl/Escape.java
@@ -1,9 +1,24 @@
 package etl;
 
+import java.util.regex.Pattern;
+
 /**
  * The Class Escape.
  */
 public class Escape {
+	
+	static final Pattern SLASH = Pattern.compile("\\\\");
+	static final Pattern CR = Pattern.compile("\\r");
+	static final Pattern LF = Pattern.compile("\\n");
+	static final Pattern DQ = Pattern.compile("\"");
+	static final Pattern SQ = Pattern.compile("\'");
+	static final Pattern TAB = Pattern.compile("\\t");
+	
+	static final Pattern S_CR = Pattern.compile("\\\\r");
+	static final Pattern S_LF = Pattern.compile("\\\\n");
+	static final Pattern S_DQ = Pattern.compile("\\\\\"");
+	static final Pattern S_SQ = Pattern.compile("\\\\\'");
+	static final Pattern S_TAB = Pattern.compile("\\\\t");
   
   /**
    * Encode.
@@ -12,11 +27,13 @@ public class Escape {
    * @return the string
    */
   public static String encode(String input) {
-    String encoded = input.replaceAll("\\r", "\\\\r");
-    encoded = encoded.replaceAll("\\n", "\\\\n");
-    encoded = encoded.replaceAll("\"", "\\\\\"");// \\\"
-    encoded = encoded.replaceAll("\'", "\\\\\'");
-    encoded = encoded.replaceAll("\t", "\\\\t");
+  	String encoded = input;
+  	encoded = SLASH.matcher(encoded).replaceAll("\\\\\\\\");
+  	encoded = CR.matcher(encoded).replaceAll("\\\\r");
+    encoded = LF.matcher(encoded).replaceAll("\\\\n");
+    encoded = DQ.matcher(encoded).replaceAll("\\\\\"");// \\\"
+    encoded = SQ.matcher(encoded).replaceAll("\\\\\'");
+    encoded = TAB.matcher(encoded).replaceAll("\\\\t");
     return encoded;
   }
   
@@ -27,14 +44,14 @@ public class Escape {
    * @return the string
    */
   public static String decodeLineBreak(String input) {
-    String decoded = input.replaceAll("\\\\r", "\r");
-    decoded = decoded.replaceAll("\\\\n", "\n");
+    String decoded = S_CR.matcher(input).replaceAll("\r");
+    decoded = S_LF.matcher(decoded).replaceAll("\n");
     return decoded;
   }
   
   public static String decodeQuote(String input) {
-    String decoded = input.replaceAll("\\\\\"", "\"");
-    decoded = decoded.replaceAll("\\\\\'", "\'");
+    String decoded = S_DQ.matcher(input).replaceAll("\"");
+    decoded = S_SQ.matcher(decoded).replaceAll("\'");
     return decoded;
   }
   
@@ -45,7 +62,7 @@ public class Escape {
    * @return the string
    */
   public static String decodeBackslashT(String input) {
-    String decoded = input.replaceAll("\\\\t", "\t");
+    String decoded = S_TAB.matcher(input).replaceAll("\t");
     return decoded;
   }
 }
